65447fce07274e60c5ebbf0661e591490b309dfc
Wafoe <43130920+Gitfoe@users.noreply.github.com>
2023-06-26 21:05:47 +0800

    Implemented initialisation > action > move transitions

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -7,15 +7,21 @@ public class BattleDialogBox : MonoBehaviour
 {
     [SerializeField] private int typeOutSpeed;
     [SerializeField] private Text dialogText;
+    [SerializeField] private GameObject actionSelector;
+    [SerializeField] private GameObject moveSelector;
+    [SerializeField] private GameObject moveDetails;
+    [SerializeField] private List<Text> actionTexts;
+    [SerializeField] private List<Text> moveTexts;
+    [SerializeField] private Text powerPointsText;
+    [SerializeField] private Text typeText;
+    [SerializeField] private Color selectedColour;
+    [SerializeField] private Color deselectedColour;
 
     /// <summary>
     /// Sets dialog text immediately.
     /// </summary>
     /// <param name="text">Text that has to be set.</param>
-    public void SetDialogText(string text)
-    {
-        dialogText.text = text;
-    }
+    public void SetDialogText(string text) => dialogText.text = text;
 
     /// <summary>
     /// Types out text based on a certain interval.
@@ -32,4 +38,28 @@ public class BattleDialogBox : MonoBehaviour
         }
     }
     
+    /// <summary>
+    /// Enables or disables the move selector and the accompanying move details.
+    /// </summary>
+    public void EnableMoveSelector(bool enabled)
+    {
+        moveSelector.SetActive(enabled);
+        moveDetails.SetActive(enabled);
+    }
+
+    public void EnableDialogText(bool enabled) => dialogText.enabled = enabled;
+
+    public void EnableActionSelector(bool enabled) => actionSelector.SetActive(enabled);
+
+    /// <summary>
+    /// Highlights the correct action selection.
+    /// </summary>
+    /// <param name="selectedAction">The currently selected action.</param>
+    public void UpdateActionSelection(int selectedAction)
+    {
+        for (var i = 0; i < actionTexts.Count; i++)
+        {
+            actionTexts[i].color = i == selectedAction ? selectedColour : deselectedColour;
+        }
+    }
 }
--- Assets/Scripts/Battle/UniteonBattle.cs
+++ Assets/Scripts/Battle/UniteonBattle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,22 +12,104 @@ public class UniteonBattle : MonoBehaviour
     [SerializeField] private UniteonHud uniteonHudGamer;
     [SerializeField] private UniteonHud uniteonHudFoe;
     [SerializeField] private BattleDialogBox battleDialogBox;
+    private BattleState _battleState;
+    private int _actionState; 
     
     // Start is called before the first frame update
     private void Start()
     {
-        InitialiseBattle();
+        StartCoroutine(InitialiseBattle());
     }
 
     /// <summary>
     /// Initializes the battle scene.
     /// </summary>
-    private void InitialiseBattle()
+    private IEnumerator InitialiseBattle()
     {
+        // Initialise battle field and Uniteons
         uniteonUnitGamer.InitialiseUniteon();
         uniteonHudGamer.SetGamerData(uniteonUnitGamer.Uniteon);
         uniteonUnitFoe.InitialiseUniteon();
         uniteonHudFoe.SetGamerData(uniteonUnitFoe.Uniteon);
-        StartCoroutine(battleDialogBox.TypeOutDialog($"A wild {uniteonUnitFoe.Uniteon.UniteonBase.UniteonName} appeared!"));
+        // Wait until wild encounter text has printed out
+        yield return StartCoroutine(battleDialogBox.TypeOutDialog($"A wild {uniteonUnitFoe.Uniteon.UniteonBase.UniteonName} appeared!"));
+        // Wait for some time after the text is done printing
+        yield return new WaitForSeconds(2f);
+        InitialiseToAction();
     }
+
+    /// <summary>
+    /// Transitions the scene state from initialisation to action.
+    /// </summary>
+    private void InitialiseToAction()
+    {
+        _battleState = BattleState.GamerAction;
+        battleDialogBox.SetDialogText("Choose a strategic move...");
+        battleDialogBox.EnableActionSelector(true);
+    }
+    
+    /// <summary>
+    /// Transitions the scene state from action to move.
+    /// </summary>
+    private void ActionToMove()
+    {
+        _battleState = BattleState.GamerMove;
+        battleDialogBox.EnableActionSelector(false);
+        battleDialogBox.EnableDialogText(false);
+        battleDialogBox.EnableMoveSelector(true);
+    }
+
+    /// <summary>
+    /// Every frame, check for a change in action selection.
+    /// </summary>
+    private void Update()
+    {
+        if (_battleState == BattleState.GamerAction)
+        {
+            HandleActionSelection();
+        }
+    }
+
+    /// <summary>
+    /// Watches for input of the player and changes the graphics accordingly.
+    /// </summary>
+    private void HandleActionSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (_actionState < 1)
+                _actionState++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (_actionState > 0)
+                _actionState--; 
+        }
+        battleDialogBox.UpdateActionSelection(_actionState);
+        // Transition to the next state if an action has been selected
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            if (_actionState == 0) // Attack
+            {
+                ActionToMove();
+            }
+            else if (_actionState == 1) // Run
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+    }
+}
+
+/// <summary>
+/// All the different states a battle could be in.
+/// </summary>
+public enum BattleState
+{
+    Start,
+    GamerAction,
+    GamerMove,
+    FoeMove,
+    Attacking
 }
